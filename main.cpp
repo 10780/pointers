3c6fa9200cbabbffca8ffa39c94e3ed2c496bd1d
10780 <9407622+10780@users.noreply.github.com>
2021-09-24 10:45:18 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,20 +5,37 @@ using namespace std;
 const int MAX = 5;
 
 int main(){
-    int var[MAX] = {10, 20, 50, 100, 200};
-    int *ptr[MAX]; //declares ptr as array of integer points
+    int var01[MAX] = {10, 20, 50, 100, 200};
+    int *ptr01[MAX]; //declares ptr as array of integer points
+
+    int var02[MAX] = {10, 20, 30, 40 , 50};
+    int *ptr02[MAX];
 
     for (int i = 0; i < MAX; i++){
-        ptr[i] = &var[i]; //assign to address of integer
+        ptr01[i] = &var01[i]; //assign to address of integer array
+        ptr02[i] = &var02[i]; //same thing as above
     }
 
-    //cycle through array
+    //cycle through first array
     for (int i = 0; i < MAX; i++){
         cout << "Value: [" << i << "] = "; //print values in array
-        cout << *ptr[i] << endl;
-        cout << "Memory location: " << ptr << "\n\n"; //print memory address of ptr
+        cout << *ptr01[i] << endl;
+        cout << "Memory location: " << ptr01 << "\n"; //print memory address of ptr
         cout << '\a'; //I like the ding lol
+
+        cin.get();
+    }
+
+    for (int i = 0; i < MAX; i++){
+
+        cout << "Value : [" << i << "] = ";
+        cout << *ptr02[i] << endl;
+        cout << "Memory location: " << ptr02 << "\n";
+        cout << '\a';
+
+        cin.get();
     }
 
     return 0;
 }
+
