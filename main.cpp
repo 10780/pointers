ceeeeacd2edfa21a4a91ed834034954b02270200
10780 <9407622+10780@users.noreply.github.com>
2021-10-13 14:51:11 -0700

    Update main.cpp
    Added more numbers and changed a few other things

--- main.cpp
+++ main.cpp
@@ -2,18 +2,21 @@
 
 //simple program for showing how pointers + arrays work
 using namespace std;
-const int MAX = 5;
+const int MAX = 10;
+string msg = "\nPress any key to keep going\n";
 
 int main(){
-    int var01[MAX] = {10, 20, 50, 100, 200};
+
+
+    int var01[MAX] = {10, 20, 50, 100, 200, 300, 400, 500, 1000, 2000};
     int *ptr01[MAX]; //declares ptr as array of integer points
 
-    int var02[MAX] = {10, 20, 30, 40 , 50};
+    int var02[MAX] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int *ptr02[MAX];
 
     for (int i = 0; i < MAX; i++){
         ptr01[i] = &var01[i]; //assign to address of integer array
-        ptr02[i] = &var02[i]; //same thing as above
+        ptr02[i] = &var02[i];
     }
 
     //cycle through first array
@@ -22,20 +25,21 @@ int main(){
         cout << *ptr01[i] << endl;
         cout << "Memory location: " << ptr01 << "\n"; //print memory address of ptr
         cout << '\a'; //I like the ding lol
-
-        cin.get();
     }
 
-    for (int i = 0; i < MAX; i++){
+    cout << msg;
+    cin.get();
 
+    for (int i = 0; i < MAX; i++){
         cout << "Value : [" << i << "] = ";
         cout << *ptr02[i] << endl;
         cout << "Memory location: " << ptr02 << "\n";
         cout << '\a';
 
-        cin.get();
+        //cin.get();
     }
 
     return 0;
 }
+
 
