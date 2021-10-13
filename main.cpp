b7ce319da42200f7a26e40e25011904d5c9ec583
10780 <9407622+10780@users.noreply.github.com>
2021-10-13 15:03:13 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,11 @@
 //simple program for showing how pointers + arrays work
 using namespace std;
 const int MAX = 10;
-string msg = "\nPress any key to keep going\n";
+string msg = "Press any key to keep going";
 
 int main(){
 
+    string *msgptr = &msg;
 
     int var01[MAX] = {10, 20, 50, 100, 200, 300, 400, 500, 1000, 2000};
     int *ptr01[MAX]; //declares ptr as array of integer points
@@ -27,7 +28,7 @@ int main(){
         cout << '\a'; //I like the ding lol
     }
 
-    cout << msg;
+    cout << "\n" << msg << "\n";
     cin.get();
 
     for (int i = 0; i < MAX; i++){
@@ -39,7 +40,10 @@ int main(){
         //cin.get();
     }
 
+    cout << "\nMemory location of msg (" << msg << "): " << msgptr << "\n";
+
     return 0;
 }
+
 
 
